c08c721ef8ca8631830aeb1d85a248f457e13dbd
GabeFlavin <51338080+GabeFlavin@users.noreply.github.com>
2024-05-11 14:45:37 -0400

    Add files via upload

---
+++ pins-config.js
@@ -0,0 +1,305 @@
+var pins_config = {
+  "pins":[
+  {
+    "shape": "square",//either "circle" or "square"
+    "hover": "<b><u>WASHINGTON DC</u></b><br>Write any text and load images<br><img src='example.png'>",//info of the popup
+    "pos_X": 585,//check the X, Y coordinates guide in the documentation
+    "pos_Y": 183,
+    "size": 18,//size of the pin
+    "outline": "#000080",//outline color of the pin
+    "upColor": "#1a1aff",//color of the pin when map load
+    "overColor": "#66d9ff",//color of the pin when mouse hover
+    "url": "https://www.html5interactivemaps.com/",//link to any webpage
+    "target": "new_window",// use "new_window", "same_window", "modal", or "none"
+    "active": true//true/false to activate/deactivate this pin
+  },
+  {
+    "shape": "circle",
+    "hover": "<b><u>LOS ANGELES</u></b><br><span style='color: #bcbcbc;'>Street Address:</span><br>&nbsp;321 Example, Address 54321<br><span style='color: #bcbcbc;'>Telephone:</span><br>&nbsp;(256) 555-4321 / (256) 555-1234",
+    "pos_X": 69,
+    "pos_Y": 260,
+    "size": 20,
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "<b><u>NEW YORK</u></b><br><span style='color: #999;'>Click to open a modal window!</span><br><span style='color: #ff6666;'><b>Modal Window Option is Compatible<br> with Bootstrap Only.</b></span>",
+    "pos_X": 616,
+    "pos_Y": 139,
+    "size": 16,
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "#mymodal",
+    "target": "modal",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "<b><u>CHICAGO</u></b><br><span style='color: #999;'>*Click to open a webpage*</span>",
+    "pos_X": 448,
+    "pos_Y": 140,
+    "size": 14,
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "HOUSTON",
+    "pos_X": 365,
+    "pos_Y": 353,
+    "size": 17,
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "PHOENIX",
+    "pos_X": 144,
+    "pos_Y": 287,
+    "size": 14,
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "SEATTLE",
+    "pos_X": 66,
+    "pos_Y": 47,
+    "size": 14,
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "DALLAS",
+    "pos_X": 358,
+    "pos_Y": 312,
+    "size": 14,
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "JACKSONVILLE",
+    "pos_X": 554,
+    "pos_Y": 330,
+    "size": 14,
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "COLUMBUS",
+    "pos_X": 513,
+    "pos_Y": 177,
+    "size": 14,
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "INDIANAPOLIS",
+    "pos_X": 474,
+    "pos_Y": 187,
+    "size": 14,
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "BLANK12",
+    "pos_X": 150,
+    "pos_Y": 400,
+    "size": 0,//change the size to display this pin
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "BLANK13",
+    "pos_X": 200,
+    "pos_Y": 400,
+    "size": 0,//change the size to display this pin
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "BLANK14",
+    "pos_X": 250,
+    "pos_Y": 400,
+    "size": 0,//change the size to display this pin
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  },
+  {
+    "shape": "circle",
+    "hover": "BLANK15",
+    "pos_X": 300,
+    "pos_Y": 400,
+    "size": 0,//change the size to display this pin
+    "outline": "#660000",
+    "upColor": "#e60000",
+    "overColor": "#ffd480",
+    "url": "https://www.html5interactivemaps.com/",
+    "target": "same_window",
+    "active": true
+  }// If you want to add more pins, you need to add comma ',' after }
+  ]
+};
+
+// The following is the script for pins interaction DON'T EDIT !!!
+function isTouchEnabled() {
+  return (("ontouchstart" in window)
+    || (navigator.MaxTouchPoints > 0)
+    || (navigator.msMaxTouchPoints > 0));
+}
+jQuery(function () {
+  var pins_len = pins_config.pins.length;
+  if(pins_len > 0) {
+    var xmlns = "http://www.w3.org/2000/svg";
+    var tsvg_obj = document.getElementById("usjspins");
+    var svg_circle, svg_rect;
+    for (var i = 0; i < pins_len; i++) {
+      if (pins_config.pins[i].shape === "circle") {
+        svg_circle = document.createElementNS(xmlns, "circle");
+        svg_circle.setAttributeNS(null, "cx", pins_config.pins[i].pos_X + 1);
+        svg_circle.setAttributeNS(null, "cy", pins_config.pins[i].pos_Y + 1);
+        svg_circle.setAttributeNS(null, "r", pins_config.pins[i].size / 2);
+        svg_circle.setAttributeNS(null, "fill", "rgba(0, 0, 0, 0.5)");
+        tsvg_obj.appendChild(svg_circle);
+        svg_circle = document.createElementNS(xmlns, "circle");
+        svg_circle.setAttributeNS(null, "cx", pins_config.pins[i].pos_X);
+        svg_circle.setAttributeNS(null, "cy", pins_config.pins[i].pos_Y);
+        svg_circle.setAttributeNS(null, "r", pins_config.pins[i].size / 2);
+        svg_circle.setAttributeNS(null, "fill", pins_config.pins[i].upColor);
+        svg_circle.setAttributeNS(null, "stroke", pins_config.pins[i].outline);
+        svg_circle.setAttributeNS(null, "stroke-width", 1);
+        svg_circle.setAttributeNS(null, "id", "usjspins_" + i);
+        tsvg_obj.appendChild(svg_circle);
+        usjsAddEvent(i);
+      }
+      else if (pins_config.pins[i].shape === "square") {
+        svg_rect = document.createElementNS(xmlns, "rect");
+        svg_rect.setAttributeNS(null, "x", pins_config.pins[i].pos_X - pins_config.pins[i].size / 2 + 1);
+        svg_rect.setAttributeNS(null, "y", pins_config.pins[i].pos_Y - pins_config.pins[i].size / 2 + 1);
+        svg_rect.setAttributeNS(null, "width", pins_config.pins[i].size);
+        svg_rect.setAttributeNS(null, "height", pins_config.pins[i].size);
+        svg_rect.setAttributeNS(null, "fill", "rgba(0, 0, 0, 0.5)");
+        tsvg_obj.appendChild(svg_rect);
+        svg_rect = document.createElementNS(xmlns, "rect");
+        svg_rect.setAttributeNS(null, "x", pins_config.pins[i].pos_X - pins_config.pins[i].size / 2);
+        svg_rect.setAttributeNS(null, "y", pins_config.pins[i].pos_Y - pins_config.pins[i].size / 2);
+        svg_rect.setAttributeNS(null, "width", pins_config.pins[i].size);
+        svg_rect.setAttributeNS(null, "height", pins_config.pins[i].size);
+        svg_rect.setAttributeNS(null, "fill", pins_config.pins[i].upColor);
+        svg_rect.setAttributeNS(null, "stroke", pins_config.pins[i].outline);
+        svg_rect.setAttributeNS(null, "stroke-width", 1);
+        svg_rect.setAttributeNS(null, "id", "usjspins_" + i);
+        tsvg_obj.appendChild(svg_rect);
+        usjsAddEvent(i);
+      }
+    }
+  }
+});
+function usjsAddEvent(id) {
+  var obj = jQuery("#usjspins_" + id);
+  if(pins_config.pins[id].active === true){
+    obj.attr({"cursor": "pointer"});
+    obj.hover(function () {
+      jQuery("#usjstip").show().html(pins_config.pins[id].hover);
+      obj.css({"fill":pins_config.pins[id].overColor});
+    }, function () {
+      jQuery("#usjstip").hide();
+      obj.css({"fill":pins_config.pins[id].upColor});
+    });
+    obj.mouseup(function(){
+      obj.css({"fill":pins_config.pins[id].overColor});
+      if (pins_config.pins[id].target === "new_window"){
+        window.open(pins_config.pins[id].url);  
+      } else if (pins_config.pins[id].target === "same_window") {
+        window.parent.location.href = pins_config.pins[id].url;
+      } else if (pins_config.pins[id].target === "modal") {
+        jQuery(pins_config.pins[id].url).modal("show");
+      }
+    });
+    obj.mousemove(function (e) {
+      var x = e.pageX + 10, y = e.pageY + 15;
+      var tipw =jQuery("#usjstip").outerWidth(), tiph =jQuery("#usjstip").outerHeight(),
+      x = (x + tipw >jQuery(document).scrollLeft() +jQuery(window).width())? x - tipw - (20 * 2) : x ;
+      y = (y + tiph >jQuery(document).scrollTop() +jQuery(window).height())? jQuery(document).scrollTop() +jQuery(window).height() - tiph - 10 : y ;
+      jQuery("#usjstip").css({left: x, top: y});
+    });
+    if (isTouchEnabled()) {
+      obj.on("touchstart", function (e) {
+        var touch = e.originalEvent.touches[0];
+        var x = touch.pageX + 10, y = touch.pageY + 15;
+        var tipw=jQuery("#usjstip").outerWidth(), tiph=jQuery("#usjstip").outerHeight(),
+        x = (x + tipw >jQuery(document).scrollLeft() +jQuery(window).width())? x - tipw -(20 * 2) : x ;
+        y =(y + tiph >jQuery(document).scrollTop() +jQuery(window).height())? jQuery(document).scrollTop() +jQuery(window).height() -tiph - 10 : y ;
+        jQuery("#usjstip").show().html(pins_config.pins[id].hover);
+        jQuery("#usjstip").css({left:x, top:y});
+      });
+      obj.on("touchend", function () {
+        jQuery("#" + id).css({"fill":pins_config.pins[id].upColor});
+        if (pins_config.pins[id].target === "new_window") {
+          window.open(pins_config.pins[id].url);
+        } else if (pins_config.pins[id].target === "same_window") {
+          window.parent.location.href = pins_config.pins[id].url;
+        } else if (pins_config.pins[id].target === "modal") {
+          jQuery(pins_config.pins[id].url).modal("show");
+        }
+      });
+    }
+  }
+}
